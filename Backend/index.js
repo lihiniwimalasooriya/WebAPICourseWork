7bcce1a2f30c2256c56737b2e87ac8fba2c0e3af
lihiniwimalasooriya <83071896+lihiniwimalasooriya@users.noreply.github.com>
2023-11-13 00:11:59 +0530

    commiy6

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,39 @@
-test
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import tourRoute from './routes/tours.js';
+import userRoute from './routes/users.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('MongoDB database connected');
+})
+.catch((err) => {
+  console.log('MongoDB database connection failed');
+});
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+// Routes
+app.use('/tours', tourRoute);
+app.use('/users', userRoute);
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
